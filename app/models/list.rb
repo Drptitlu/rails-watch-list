e2619a701f2704e79946f7c233ad4ba86e7cb341
Fabien alexandre <alexandre.fabien@gmail.com>
2022-02-17 12:05:04 +0100

    models fait

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,5 @@
 class List < ApplicationRecord
+  has_many :bookmarks
+  has_many :movies, through: :bookmarks, dependent: :destroy
+  validates :name, :presence => { :message => " cannot be blank" }, uniqueness: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  validates :title, :presence => { :message => " cannot be blank" }, uniqueness: true
+  validates :overview, :presence => { :message => " cannot be blank" }
 end
