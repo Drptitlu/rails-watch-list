18ba65dd2bcae6cf919641edbcb215915455cbef
Fabien alexandre <alexandre.fabien@gmail.com>
2022-02-17 14:39:06 +0100

    bookmark fait

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,5 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, :presence => { :message => " cannot be blank" }, uniqueness: true
   validates :overview, :presence => { :message => " cannot be blank" }
+  accepts_nested_attributes_for :bookmarks, allow_destroy: true
 end
