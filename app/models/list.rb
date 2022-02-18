3667b6d0731ae6b2648aba00c20a9fa19f871b5c
Fabien alexandre <alexandre.fabien@gmail.com>
2022-02-18 12:50:35 +0100

    add photo

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, :presence => { :message => " cannot be blank" }, uniqueness: true
+  has_one_attached :photo
 end
